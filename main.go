2e3b92b0a5219811b3a81103a323f5d5eea962ec
dawwson <shines1427@gmail.com>
2024-06-26 18:29:47 +0900

    Add: #5.2 mvp.css 적용

--- main.go
+++ main.go
@@ -17,9 +17,9 @@ type homeData struct {
 }
 
 // NOTE: w - response, r - request(포인터, 실제 reqeust 사용)
+// NOTE: Must - 에러가 발생하면 출력해줌
 func home(w http.ResponseWriter, r *http.Request) {
-	// NOTE: Must - 에러가 발생하면 출력해줌
-	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
 	data := homeData{"Home", blockchain.GetBlockChain().GetAllBlocks()}
 	tmpl.Execute(w, data)
 }
