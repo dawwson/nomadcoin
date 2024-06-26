52171852c48914baf38f1f7f4c03756603c4b212
dawwson <shines1427@gmail.com>
2024-06-26 15:52:32 +0900

    Feat: #4.1 블록 생성

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type Block struct {
+	data string
+	hash string
+	prevHash string
+}
 
 func main() {
-	fmt.Println("Welcom to Nomad Coin!");
+	// 1. 새로 추가할 블록을 생성한다.
+	block := Block{"data", "", ""}
+	// 2. [data + 이전 해시]를 byte 타입의 slice로 변환하여 해싱한다.
+	hash := sha256.Sum256([]byte(block.data + block.prevHash))
+	// 3. 해시를 16진수 string으로 변환한다.
+	hexHash := fmt.Sprintf("%x", hash)
+	// 4. 블록의 해시에 저장한다.
+	block.hash = hexHash
+	fmt.Println(block)
 }
