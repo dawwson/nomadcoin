c81db24dbc31bbb83dc8f183086e6f94c8206493
dawwson <shines1427@gmail.com>
2024-06-26 17:53:24 +0900

    Feat: #5.1 템플릿 연결

--- main.go
+++ main.go
@@ -4,16 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"text/template"
+
+	"github.com/dawwson/nomadcoin/blockchain"
 )
 
 const port string = ":3000"
 
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+// NOTE: w - response, r - request(포인터, 실제 reqeust 사용)
+func home(w http.ResponseWriter, r *http.Request) {
+	// NOTE: Must - 에러가 발생하면 출력해줌
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockChain().GetAllBlocks()}
+	tmpl.Execute(w, data)
+}
+
 func main() {
 	// GET /
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// NOTE: w - response, r - request(포인터, 실제 reqeust 사용)
-		fmt.Fprint(w, "Hello from home!")
-	})
+	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
