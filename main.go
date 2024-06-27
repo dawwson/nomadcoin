ca9b23ac698c78e52978829ef6d9d4584eb4b892
dawwson <shines1427@gmail.com>
2024-06-27 11:22:05 +0900

    Feat: #5.3 partials로 컴포넌트 분리

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ import (
 	"github.com/dawwson/nomadcoin/blockchain"
 )
 
-const port string = ":3000"
+const (
+	port        string = ":3000"
+	templateDir string = "templates/"
+)
+
+var templates *template.Template
 
 type homeData struct {
 	PageTitle string
@@ -17,15 +22,17 @@ type homeData struct {
 }
 
 // NOTE: w - response, r - request(포인터, 실제 reqeust 사용)
-// NOTE: Must - 에러가 발생하면 출력해줌
 func home(w http.ResponseWriter, r *http.Request) {
-	tmpl := template.Must(template.ParseFiles("templates/pages/home.gohtml"))
 	data := homeData{"Home", blockchain.GetBlockChain().GetAllBlocks()}
-	tmpl.Execute(w, data)
+	templates.ExecuteTemplate(w, "home", data)
 }
 
 func main() {
-	// GET /
+	// load templates
+	// NOTE: Must - 에러가 발생하면 출력해줌
+	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
+	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
+
 	http.HandleFunc("/", home)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
