b810a06c955ce69fa0918da3850ac127630d5555
dawwson <shines1427@gmail.com>
2024-06-27 11:41:27 +0900

    Feat: #5.4 새로운 블록 추가 기능

--- main.go
+++ main.go
@@ -27,6 +27,18 @@ func home(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "home", data)
 }
 
+func add(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		templates.ExecuteTemplate(w, "add", nil)
+	case "POST": 
+		r.ParseForm()
+		data := r.Form.Get("blockData")
+		blockchain.GetBlockChain().AddBlock(data)
+		http.Redirect(w, r, "/", http.StatusPermanentRedirect)
+	}
+}
+
 func main() {
 	// load templates
 	// NOTE: Must - 에러가 발생하면 출력해줌
@@ -34,6 +46,7 @@ func main() {
 	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
 
 	http.HandleFunc("/", home)
+	http.HandleFunc("/add", add)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
