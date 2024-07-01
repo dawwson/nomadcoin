5fef8309ca919012feb45b16d13d54a9ed66b5a6
dawwson <shines1427@gmail.com>
2024-07-01 12:09:53 +0900

    Feat: #6.0~6.1 struct field tag 적용

--- main.go
+++ main.go
@@ -1,7 +1,41 @@
 package main
 
-import "github.com/dawwson/nomadcoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+const port string = ":3000"
+
+type URLDescription struct {
+	// NOTE: struct field tag - struct가 json으로 변환될 때 tag에 지정한 이름으로 변경됨
+	URL         string `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"` // omitempty: 값이 비어있으면 필드 제거
+}
+
+func documentation(w http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL: "/",
+			Method: "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL: "/blocks",
+			Method: "POST",
+			Description: "Add a Block",
+			Payload: "data:string",
+		},
+	}
+	w.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(data)
+}
 
 func main() {
-	explorer.Start();
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
