59e94f9fa0037df4e8d23f517c6b2b9a74b56f6e
dawwson <shines1427@gmail.com>
2024-07-01 18:50:23 +0900

    Feat: #6.2 인터페이스 구현으로 JSON으로 마샬링 시 URL 값  변환

--- main.go
+++ main.go
@@ -6,11 +6,25 @@ import (
 	"log"
 	"net/http"
 )
-const port string = ":3000"
+
+const (
+	port string = ":3000"
+	baseURL string = "http://localhost"
+)
+
+type URL string
+
+// NOTE: https://pkg.go.dev/encoding#TextMarshaler
+// TextMarshaler 인터페이스를 implements 키워드 없이 암묵적으로 구현합니다.
+// URL이 어떻게 JSON으로 마샬링 될 지를 구현합니다.
+func (path URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("%s%s%s", baseURL, port, path)
+	return []byte(url), nil
+}
 
 type URLDescription struct {
 	// NOTE: struct field tag - struct가 json으로 변환될 때 tag에 지정한 이름으로 변경됨
-	URL         string `json:"url"`
+	URL         URL `json:"url"`
 	Method      string `json:"method"`
 	Description string `json:"description"`
 	Payload     string `json:"payload,omitempty"` // omitempty: 값이 비어있으면 필드 제거
@@ -19,12 +33,12 @@ type URLDescription struct {
 func documentation(w http.ResponseWriter, r *http.Request) {
 	data := []URLDescription{
 		{
-			URL: "/",
+			URL: URL("/"),
 			Method: "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL: "/blocks",
+			URL: URL("/blocks"),
 			Method: "POST",
 			Description: "Add a Block",
 			Payload: "data:string",
