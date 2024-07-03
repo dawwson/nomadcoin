9b7ea678bf53201e250db68cc8692d8107f6a938
dawwson <shines1427@gmail.com>
2024-07-03 10:22:31 +0900

    Refactor: #7.3 Flag로 CLI 리팩터

--- main.go
+++ main.go
@@ -4,35 +4,36 @@ import (
 	"flag"
 	"fmt"
 	"os"
+
+	"github.com/dawwson/nomadcoin/explorer"
+	"github.com/dawwson/nomadcoin/rest"
 )
 
 func usage() {
 	fmt.Printf("Welcome to Nomad Coin\n\n")
-	fmt.Printf("Please use the following commands\n\n")
-	fmt.Printf("✅ explorer: Start the HTML Explorer\n")
-	fmt.Printf("✅ rest    : Start the REST API (recommended)\n\n")
+	fmt.Printf("Please use the following flags\n\n")
+	fmt.Printf("✅ -port:   Set the PORT of the server\n")
+	fmt.Printf("✅ -mode:   Choose between 'html' and 'rest'\n\n")
 	os.Exit(0)
 }
 
 func main() {
-	if len(os.Args) < 2 {
+	if len(os.Args) == 1 {
 		usage()
 	}
 
-	rest := flag.NewFlagSet("rest", flag.ExitOnError)
-	portFlag := rest.Int("port", 4000, "Sets the port of the server")
+	port := flag.Int("port", 4000, "Set port of the server")
+	mode := flag.String("mode", "rest", "Choose between 'html' and 'rest'")
 
-	switch os.Args[1] {
-	case "explorer":
-		fmt.Println("Start HTML Explorer")
+	flag.Parse()
+
+	switch *mode {
 	case "rest":
-		rest.Parse(os.Args[2:])
+		rest.Start(*port)
+	case "html":
+		explorer.Start(*port)
 	default:
 		usage()
 	}
-	
-	if rest.Parsed() {
-		fmt.Println(portFlag)
-		fmt.Println("Start server")
-	}
+	fmt.Println(*port, *mode)
 }
