7c6d776c7848330d3e4bd97bba9bbc67806c6ebf
dawwson <shines1427@gmail.com>
2024-07-06 15:21:50 +0900

    Feat: #9.2 블록 추가할 때 채굴 과정 추가

--- blockchain/block.go
+++ blockchain/block.go
@@ -4,10 +4,13 @@ import (
 	"crypto/sha256"
 	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/dawwson/nomadcoin/db"
 	"github.com/dawwson/nomadcoin/utils"
 )
+
+const difficulty int = 2 // 0이 2개로 시작하는 hash를 찾는다.
 
 type Block struct {
 	Data       string `json:"data"`
@@ -28,19 +31,42 @@ func (b *Block) restore(data []byte) {
 	utils.FromBytes(b, data)
 }
 
+// 마이닝 : 조건(difficulty 만큼의 0으로 시작)에 맞는 해시를 생성하는 nonce를 찾는다.
+func (b *Block) mine() {
+	target := strings.Repeat("0", difficulty)
+
+	for {
+		// block을 string으로 변환하여 해시 생성
+		blockAsString := fmt.Sprint(b)
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
+		fmt.Printf("Block as String: %s\nHash: %s\nTarget: %s\nNonce: %d\n\n\n", blockAsString, hash, target, b.Nonce)
+
+		// hash의 prefix가 target이면 그 해시를 블록의 해시로 지정
+		if strings.HasPrefix(hash, target) {
+			b.Hash = hash
+			break
+		} else {
+			b.Nonce++
+		}
+	}
+}
+
 // 블록 생성
 func createBlock(data string, prevHash string, height int) *Block {
 	// 1. 블록 객체 생성
 	block := Block{
-		Data:     data,
-		Hash:     "",
-		PrevHash: prevHash,
-		Height:   height,
+		Data:       data,
+		Hash:       "",
+		PrevHash:   prevHash,
+		Height:     height,
+		Difficulty: difficulty,
+		Nonce:      0,
 	}
 
 	// 2. 해시 생성
-	payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
-	block.Hash = fmt.Sprintf("%x", sha256.Sum256(([]byte(payload))))
+	// payload := block.Data + block.PrevHash + fmt.Sprint(block.Height)
+	// block.Hash = fmt.Sprintf("%x", sha256.Sum256(([]byte(payload))))
+	block.mine()
 
 	// 3. DB에 블록 저장
 	block.persist()
--- main.go
+++ main.go
@@ -8,4 +8,21 @@ import (
 func main() {
 	defer db.Close()
 	cli.Start()
+
+	// difficulty := 5
+	// target := strings.Repeat("0", difficulty)
+	// nonce := 1
+
+	// for {
+	// 	hash := fmt.Sprintf("%x", sha256.Sum256([]byte("Hello"+fmt.Sprint(nonce))))
+
+	// 	fmt.Printf("Hash: %s\nTarget: %s\nNonce: %d\n\n", hash, target, nonce)
+
+	// 	if strings.HasPrefix(hash, target) {
+	// 		return
+	// 	} else {
+	// 		// 조건이 맞지 않으면 nonce를 늘려서 반복
+	// 		nonce++
+	// 	}
+	// }
 }
